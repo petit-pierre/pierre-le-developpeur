f526564f4612fc7e3d847df8d51004df0d85bace
petit-pierre <aubree.pierre2@gmail.com>
2024-02-13 16:01:05 +0100

    V0.0.4 premier probleme : mongoose et mongodb

--- back/app.js
+++ back/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://aubree-pierre:<jqdsm67hcdfsjmiU7>@cluster0.6mhsrgi.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use(express.json());
 
