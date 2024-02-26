36f8fb7e0cde200e5324433323c5845b9d3d6bc4
petit-pierre <aubree.pierre2@gmail.com>
2024-02-26 12:11:45 +0100

    V0.3.4 socket io

--- back/server.js
+++ back/server.js
@@ -1,5 +1,10 @@
+const express = require("express");
 const http = require("http");
 const app = require("./app");
+const { Server } = require(`socket.io`);
+const cors = require("cors");
+
+app.use(cors());
 
 const normalizePort = (val) => {
   const port = parseInt(val, 10);
@@ -46,3 +51,23 @@ server.on("listening", () => {
 });
 
 server.listen(port);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST", "PUT"],
+  },
+});
+
+io.on("connection", (socket) => {
+  //console.log(`User Connected: ${socket.id}`);
+
+  /*socket.on("join_room", (data) => {
+    socket.join(data);
+  });*/
+
+  socket.on("send_message", (data) => {
+    //socket.to(data.room).emit("receive_message", data);
+    socket.broadcast.emit("receive_message", data);
+  });
+});
--- pierre-le-developpeur/src/pages/Home/index.jsx
+++ pierre-le-developpeur/src/pages/Home/index.jsx
@@ -1,13 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import io from "socket.io-client";
 
 function Home() {
+  const socket = io.connect("http://localhost:3000");
+
   const skills = useSelector((state) => state.data.skills);
   const navigate = useNavigate();
   const [lang, setLang] = useState("en");
+
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+
   const { t, i18n } = useTranslation();
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
@@ -35,6 +52,18 @@ function Home() {
         )}
         <button onClick={() => english()}>English</button>
         <button onClick={() => francais()}>fr</button>
+        <div>
+          {" "}
+          <input
+            placeholder="Message..."
+            onChange={(event) => {
+              setMessage(event.target.value);
+            }}
+          />
+          <button onClick={sendMessage}> Send Message</button>
+          <h1> Message:</h1>
+          {messageReceived}
+        </div>
       </main>
     );
   } else {
