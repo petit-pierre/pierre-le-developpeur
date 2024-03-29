03e7bcd5d196049303648545ec4157f3366c6b14
petit-pierre <aubree.pierre2@gmail.com>
2024-03-29 10:51:12 +0100

    V0.9.3 composant bloc de texte dynamique fonctionel

--- pierre-le-developpeur/src/components/TextArea/index.jsx
+++ pierre-le-developpeur/src/components/TextArea/index.jsx
@@ -1,15 +1,66 @@
 import { useSelector } from "react-redux";
 import "./textArea.css";
+import { useEffect } from "react";
 
 function TextArea({ props }) {
   const language = useSelector((state) => state.data.language);
-  //console.log(french);
+
+  //window.addEventListener("resize", console.log("coucou"));
+
+  function setTailleHeight(oEvent) {
+    var iHeight = "auto";
+    this.style.height = iHeight;
+    if (this.scrollHeight > 0) {
+      iHeight = this.scrollHeight + "px";
+      this.style.height = iHeight;
+    }
+    if (this.value === "") {
+      this.style.removeProperty("height");
+    }
+  }
+
+  function handleResize() {
+    var oForm = document.forms["myForm"],
+      aoTextArea = oForm.getElementsByClassName("wdgAutoSize");
+    for (let oTextArea of aoTextArea) {
+      /* vous pouver aussi utiliser l'event keyup */
+      oTextArea.addEventListener("input", setTailleHeight);
+      //si le textarea n'est pas vide au chargement de la page
+      setTailleHeight.call(oTextArea, true);
+    }
+  }
+
+  useEffect(() => {
+    handleResize();
+  }, []);
+
+  useEffect(() => {
+    // Attach the event listener to the window object
+    window.addEventListener("resize", handleResize);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    handleResize();
+  }, [language]);
+
   return (
     <div className="textArea">
-      <textarea
-        disabled={true}
-        placeholder={language === "FR" ? props.french : props.english}
-      ></textarea>
+      <form id="myForm" className="mise-en-page">
+        <textarea
+          id="champText"
+          className="wdgAutoSize"
+          spellCheck={false}
+          readOnly={true}
+          value={language === "FR" ? props.french : props.english}
+        >
+          {}
+        </textarea>
+      </form>
     </div>
   );
 }
